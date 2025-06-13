ff5a912b167947f3299062b8f7fa47320b1c7052
Martin <martkeu@web.de>
2025-06-13 12:58:32 +0200

    create simple switch statement

--- js-conditions-and-booleans/js-conditions-and-booleans_weather-check/js/index.js
+++ js-conditions-and-booleans/js-conditions-and-booleans_weather-check/js/index.js
@@ -3,3 +3,17 @@ const weather = "rainy"
 
 // Adjust the "temperature" value to trigger different messages (for Part 2 only).
 const temperature = 22; 
+
+switch (weather) {
+    case "sunny":
+        console.log("It's sunny!");
+        break;
+    case "rainy":
+        console.log("It's raining, don't forget your umbrella!");
+        break;
+    case "snowy":
+        console.log("It's snowing, stay warm!");
+        break;
+    default:
+        console.log("II'm not sure what the weather is like.");
+}
