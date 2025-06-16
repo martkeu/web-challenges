21cfcb38cb79f971a2ff05f2de58106fd3888777
Martin <martkeu@web.de>
2025-06-16 11:49:07 +0200

    solve functions-with-parameters-extra

--- js-functions/js-functions_functions-with-parameters-extra/js/index.js
+++ js-functions/js-functions_functions-with-parameters-extra/js/index.js
@@ -6,7 +6,9 @@ console.clear();
    The formula is: square = number * number
 */
 
-function printSquare(number) {}
+function printSquare(number) {
+	console.log(number * number);
+}
 
 printSquare(3);
 printSquare(5);
@@ -19,11 +21,14 @@ printSquare(5);
 
 // Uncomment the following function calls and implement the function `printCircumference`.
 
-// printCircumference(4);
-// printCircumference(6);
+function printCircumference(radius) {
+	console.log(2 * Math.PI * radius);
+}
+printCircumference(4);
+printCircumference(6);
 
 /*
-3: Write a function that accepts the width and length of a rectangle
+3: Write a function that accepts the width clnd length of a rectangle
    and logs the following text to the console: "The area of the rectangle is ?".
    The function should replace the question mark "?" with the correct area.
    The function does not exist yet.
@@ -32,5 +37,9 @@ printSquare(5);
 
 // Uncomment the following function calls and implement the function `printRectangleArea`.
 
-// printRectangleArea(5, 7);
-// printRectangleArea(3, 4);
+function printRectangleArea(width, length) {
+	console.log(`The area of the rectangle is ${width * length}`);
+}
+
+printRectangleArea(5, 7);
+printRectangleArea(3, 4);
