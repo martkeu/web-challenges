c9695273486fee448402dbd2ee0b3b43a752c79d
Martin <martkeu@web.de>
2025-06-17 22:44:53 +0200

    solve calculator

--- js-forms/js-forms_calculator/js/index.js
+++ js-forms/js-forms_calculator/js/index.js
@@ -3,30 +3,56 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const resultOutput = document.querySelector('[data-js="result"]');
 
-function add(a, b) {
-  return a + b;
-}
+// function add(a, b) {
+//   return a + b;
+// }
 
-function subtract(a, b) {
-  return a - b;
-}
+// function subtract(a, b) {
+//   return a - b;
+// }
 
-function multiply(a, b) {
-  return a * b;
-}
+// function multiply(a, b) {
+//   return a * b;
+// }
 
-function divide(a, b) {
-  return a / b;
-}
+// function divide(a, b) {
+//   return a / b;
+// }
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
 
-  let result;
+	let result;
 
-  // --v-- write your code here --v--
+	// --v-- write your code here --v--
+	const formData = new FormData(event.target);
+	const data = Object.fromEntries(formData);
 
-  // --^-- write your code here --^--
+	const operandA = Number(data.numberA);
+	const operandB = Number(data.numberB);
+	const operator = data.operator;
 
-  resultOutput.textContent = result;
+	console.log({ operandA, operandB, operator });
+
+	switch (operator) {
+		case 'addition':
+			result = operandA + operandB;
+			break;
+		case 'subtraction':
+			result = operandA - operandB;
+			break;
+		case 'multiplication':
+			result = operandA * operandB;
+			break;
+		case 'division':
+			if (operandB !== 0) {
+				result = operandA / operandB;
+			} else {
+				result = 'Dividing by 0 is not allowed!';
+			}
+			break;
+	}
+	// --^-- write your code here --^--
+
+	resultOutput.textContent = result;
 });
