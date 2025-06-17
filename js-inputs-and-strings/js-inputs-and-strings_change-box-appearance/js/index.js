bfbfa903c88184f8254a8628cc8693a8190de99b
Martin <martkeu@web.de>
2025-06-17 12:24:14 +0200

    solve box-appearance

--- js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,1 +1,19 @@
 console.clear();
+
+const color = document.querySelector('[data-js="input-color"]');
+const radius = document.querySelector('[data-js="input-radius"]');
+const rotation = document.querySelector('[data-js="input-rotation"]');
+
+const box = document.querySelector('[data-js="box"]');
+
+color.addEventListener('input', () => {
+    box.style.backgroundColor = `hsl(${color.value}, 100%, 50%)`;
+})
+
+radius.addEventListener('input', () => {
+    box.style.borderRadius = `${radius.value}%`;
+})
+
+rotation.addEventListener('input', () => {
+    box.style.transform = `rotate(${rotation.value}deg)`;
+})
