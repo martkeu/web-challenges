43c9a0f8069d6dba9831ae5ba4c6139b26d8f4dc
Martin <martkeu@web.de>
2025-06-17 15:52:34 +0200

    solve handle-form-submit

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,38 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+const firstNameField = document.querySelector('#first-name');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formElements = event.target.elements;
+
+    const firstName = formElements.firstName.value;
+    const lastName = formElements.lastName.value;
+    const age = formElements.age.value;
+    const email = formElements.email.value;
+    const complaint = formElements.complaint.value;
+    const details = formElements.details.value;
+    const badness = formElements.badness.value;
+    const orderDate = formElements.orderDate.value;
+    const tos = formElements.tos.value;
+
+    form.reset();   
+    firstNameField.focus();
+
+    console.log({
+        firstName,
+        lastName,
+        age,
+        email,
+        complaint,
+        details,
+        badness,
+        orderDate,
+        tos
+    });
+
+    console.log(`The age-badness-sum of ${firstName} is ${Number(age) + Number(badness)}.`);
+});
+
