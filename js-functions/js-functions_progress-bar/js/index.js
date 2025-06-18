5ad3a4d4d467d50d2334d2aa1798cd33c75b2d5f
Martin <martkeu@web.de>
2025-06-18 12:04:01 +0200

    provide challenges js-create-element

--- js-functions/js-functions_progress-bar/js/index.js
+++ js-functions/js-functions_progress-bar/js/index.js
@@ -2,4 +2,15 @@ console.clear();
 
 const progressBar = document.querySelector('[data-js="progress-bar"]');
 
-function calculateScrollPercentage() {}
+function calculateScrollPercentage() {
+    return window.scrollY / (document.body.clientHeight - window.innerHeight) * 100;
+}
+ 
+document.addEventListener('scroll', () => {
+    progressBar.style.width = `${calculateScrollPercentage()}%`;
+});
+
+
+// window.scrollY: The Y position of the window on the total webpage.
+// window.innerHeight: The height of the visible part of the window.
+// document.body.clientHeight : The total height of the webpage.
