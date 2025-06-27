cbb496a45156356bbc867a6d064e97925df1f5ae
Martin <martkeu@web.de>
2025-06-27 17:21:55 +0200

    solve destructuring-objects

--- js-modern-syntax/js-modern-syntax_destructuring-objects/js/index.js
+++ js-modern-syntax/js-modern-syntax_destructuring-objects/js/index.js
@@ -7,12 +7,13 @@ Make sure to export your variable as shown in the example below for the tests to
 */
 
 const company = {
-  name: "TechAcademy",
-  course: "Web Development",
-  location: "Hamburg",
+	name: 'TechAcademy',
+	course: 'Web Development',
+	location: 'Hamburg',
 };
 
 // Example: export const { value1 } = myObject;
+export const { course } = company;
 
 /*
 EXERCISE 2
@@ -24,7 +25,9 @@ Use destructuring to extract the following:
       Set a default value of `false` if `isAdmin` is not present.
 */
 
-const user = { name: "John", years: 30 };
+const user = { name: 'John', years: 30 };
+
+export const { name, years: age, isAdmin = false } = user;
 
 /*
 EXERCISE 3
@@ -33,10 +36,12 @@ rename them as `dogName`, `dogBreed`, and `dogAge`, respectively.
 */
 
 const dog = {
-  name: "Pluto",
-  breed: "husky",
-  age: 5,
+	name: 'Pluto',
+	breed: 'husky',
+	age: 5,
 };
+
+export const { name: dogName, breed: dogBreed, age: dogAge } = dog;
 
 /*
 EXERCISE 4
@@ -45,10 +50,12 @@ Store all other properties in an object called `moreInformation`.
 */
 
 const person = {
-  lastName: "Main",
-  age: 24,
-  firstName: "Alex",
+	lastName: 'Main',
+	age: 24,
+	firstName: 'Alex',
 };
+
+export const { lastName: personLastName, ...moreInformation } = person;
 
 /*
 EXERCISE 5
@@ -57,15 +64,19 @@ three variables `name`, `country` and `numPeople`.
 Hint: You may need to rename one property during destructuring.
 */
 
-export function logInfo(city) {
-  const name = city.name;
-  const country = city.country;
-  const numPeople = city.population;
+// export function logInfo(city) {
+// 	const name = city.name;
+// 	const country = city.country;
+// 	const numPeople = city.population;
 
-  return `${name} is in ${country} and has ${numPeople} inhabitants in it.`;
+// 	return `${name} is in ${country} and has ${numPeople} inhabitants in it.`;
+// }
+
+export function logInfo(city) {
+    const { name, country, population: numPeople } = city;
+
+	return `${name} is in ${country} and has ${numPeople} inhabitants in it.`;
 }
 
 // Usage example:
-console.log(
-  logInfo({ name: "Marseille", country: "France", population: 861635 })
-);
+console.log(logInfo({ name: 'Marseille', country: 'France', population: 861635 }));
