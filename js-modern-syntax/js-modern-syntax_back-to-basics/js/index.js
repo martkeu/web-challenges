be03a17d642ee6b8b7aa284d31029a9d9cab7403
Martin <martkeu@web.de>
2025-06-29 00:25:05 +0200

    solve back-to-basics

--- js-modern-syntax/js-modern-syntax_back-to-basics/js/index.js
+++ js-modern-syntax/js-modern-syntax_back-to-basics/js/index.js
@@ -3,6 +3,7 @@ Now that you've practiced destructuring, default parameters, and the spread oper
 try reversing the challenge by rewriting this code without using these modern features.
 */
 
+/*
 export const getNameAndCountry = ({ name, country }) => [name, country];
 
 export const getRelocatedCity = (
@@ -14,4 +15,22 @@ export const getRelocatedCity = (
     ...city1,
     country,
   };
+};
+*/
+
+export const getNameAndCountry = (obj) => [obj.name, obj.country];
+
+export const getRelocatedCity = (
+  city1,
+  city2 = { name: "Berlin", country: "Germany" }
+) => {
+    let returnObject = {};
+
+    for (let [key, val] of Object.entries(city1)) {
+        returnObject[key] = val;
+    }
+    
+    returnObject.country = getNameAndCountry(city2)[1];
+    
+    return returnObject;
 };
