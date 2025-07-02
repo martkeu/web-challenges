96ff96eb52642bf695bb51da52704beda5798bec
Martin <martkeu@web.de>
2025-07-02 12:06:05 +0200

    solve attributes

--- react-basics/react-basics_attributes/src/App.jsx
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,25 @@
-import "./styles.css";
+import './styles.css';
 
 export default function App() {
-  return <article>Replace me with an article</article>;
+	return <Article />;
+}
+
+function Article() {
+	return (
+		<article className="article">
+			<h2 className="article__title">Titel</h2>
+
+			<form>
+				<label htmlFor="mainArticle">Label</label>
+				<input type="text" id="mainArticle" />
+			</form>
+
+			<a
+				href="https://de.wikipedia.org/wiki/Friedrich_Nietzsche"
+				className="article__link"
+			>
+				Friedrich Nietzsche
+			</a>
+		</article>
+	);
 }
