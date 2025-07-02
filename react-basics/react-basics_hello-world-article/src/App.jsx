74cfdc78b341adfaf1993a8b4edae906ed91644e
Martin <martkeu@web.de>
2025-07-02 11:41:30 +0200

    solve hello-world-article

--- react-basics/react-basics_hello-world-article/src/App.jsx
+++ react-basics/react-basics_hello-world-article/src/App.jsx
@@ -1,5 +1,20 @@
-import "./styles.css";
+import './styles.css';
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+    //return <div>Replace me with your Component!</div>;
+    
+	return <HelloWorldArticle />;
+}
+
+function HelloWorldArticle() {
+	return (
+		<article>
+            <h1>Titel</h1>
+            
+			<h2>Untertitel</h2>
+
+			<p>Absatz 1</p>
+			<p>Absatz 2</p>
+		</article>
+	);
 }
