f715800d121da0384b796805a6da7ca2089aa313
Martin <martkeu@web.de>
2025-07-02 17:49:49 +0200

    solve challenge react-props_buttons

--- react-props/react-props_buttons/src/App.jsx
+++ react-props/react-props_buttons/src/App.jsx
@@ -1,3 +1,36 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+	function handleClick() {
+		alert('You clicked me!');
+	}
+
+	return (
+		<>
+			<Button
+				color="green"
+				disabled={false}
+				text="Click me"
+				onClick={handleClick}
+			/>
+
+            <Button
+                color="red"
+                disabled={true}
+                text="Vergeblich"
+            />
+		</>
+	);
+}
+
+
+function Button({ color, disabled, text, onClick }) {
+	return (
+		<button
+			style={{ backgroundColor: color, color: 'white' }}
+			// onClick={() => alert('You clicked me!')}
+			onClick={() => onClick()}
+			disabled={disabled}
+		>
+			{text}
+		</button>
+	);
 }
