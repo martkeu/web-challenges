654ae60ea2a92e3cc11ce605e1f9ee2f711040c5
Martin <martkeu@web.de>
2025-07-02 15:34:02 +0200

    solve challenge react-props_greeting

--- react-props/react-props_greeting/src/App.jsx
+++ react-props/react-props_greeting/src/App.jsx
@@ -1,3 +1,18 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+    //   return <h1>Replace me with your component!</h1>;
+    return (
+        <>
+            <Greeting name="Erwin" />
+
+            <Greeting name="Florian" />
+            <Greeting name="Felix" />
+            <Greeting name="Sven" />
+        </>
+    );
+}
+
+function Greeting({ name }) {
+    const coaches = ['Florian', 'Felix', 'Sven'];
+
+    return <h1>Hello, {coaches.includes(name) ? `Coach ${name}` : name}!</h1>;
 }
