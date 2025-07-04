5846e6f030b9de3122fb3ef8082b7ff793eb09f5
Martin <martkeu@web.de>
2025-07-04 10:49:44 +0200

    solve challenge nesting_buttons-and-children

--- react-nesting/react-nesting_buttons-and-children/src/App.jsx
+++ react-nesting/react-nesting_buttons-and-children/src/App.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import "./styles.css";
+import React from 'react';
+import './styles.css';
 
 export default function App() {
-  return (
-    <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-    </main>
-  );
+	return (
+		<main>
+			<Button>Click me!</Button>
+			<Button>Absenden</Button>
+			<Button>Registrieren</Button>
+			<Button>Anmelden</Button>
+		</main>
+	);
 }
 
-function Button() {
-  return (
-    <button className="button" type="button">
-      Click me!
-    </button>
-  );
+function Button({ children }) {
+	return (
+		<button className="button" type="button">
+			{children}
+		</button>
+	);
 }
