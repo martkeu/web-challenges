b5986144b3e9d26ea0ca88c01eb1fd6e0400899b
Martin <martkeu@web.de>
2025-07-07 21:07:10 +0200

    journal-app-entries-array improved

--- react-with-arrays/react-with-arrays_journal-app-entries-array/src/components/EntriesSection/index.jsx
+++ react-with-arrays/react-with-arrays_journal-app-entries-array/src/components/EntriesSection/index.jsx
@@ -43,37 +43,22 @@ export default function EntriesSection() {
 					Favorites <Badge>1</Badge>
 				</Tab>
 			</Tabs>
-			<div className="entries-section__entries">
+
+			<ul className="entries-section__entries">
 				{entries.map((entry, index) => (
-					<>
+					<li key={entry.id}>
+                        {index > 0 && <Divider />}
+                        
 						<Entry
-							key={entry.id}
 							date={entry.date}
 							motto={entry.motto}
 							notes={entry.notes}
-						/>
-                        {(index !== entries.length - 1) ? < Divider /> : ''}
-					</>
+                        />
+                        
+						{/* {(index !== entries.length - 1) ? <Divider /> : null} */}
+					</li>
 				))}
-
-				{/* <Entry
-					date="Feb 27, 2028"
-					motto="Thats life in the city"
-					notes="Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si mihi. Et quidem se repellere, idque instituit docere sic omne animal, simul atque."
-				/>
-				<Divider />
-				<Entry
-					date="Feb 26, 2028"
-					motto="Thats life in the city"
-					notes="Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si mihi. Et quidem se repellere, idque instituit docere sic omne animal, simul atque."
-				/>
-				<Divider />
-				<Entry
-					date="Feb 25, 2028"
-					motto="Thats life in the city"
-					notes="Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si mihi. Et quidem se repellere, idque instituit docere sic omne animal, simul atque."
-				/> */}
-			</div>
+			</ul>
 		</section>
 	);
 }
