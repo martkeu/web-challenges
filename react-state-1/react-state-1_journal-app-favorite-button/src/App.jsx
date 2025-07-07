6fca9264b4b917fdae7b3610730922017a9fbe82
Martin <martkeu@web.de>
2025-07-07 07:44:34 +0200

    solve challenge state-1_journal-app-favorite-button

--- react-state-1/react-state-1_journal-app-favorite-button/src/App.jsx
+++ react-state-1/react-state-1_journal-app-favorite-button/src/App.jsx
@@ -1,20 +1,22 @@
-import "./App.css";
-import EntriesSection from "./components/EntriesSection";
-import EntryForm from "./components/EntryForm";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+import './App.css';
+import EntriesSection from './components/EntriesSection';
+import EntryForm from './components/EntryForm';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
-  return (
-    <div className="app">
-      <Header />
-      <main className="app__main">
-        <EntryForm />
-        <EntriesSection />
-      </main>
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="app">
+			<Header />
+
+			<main className="app__main">
+				<EntryForm />
+				<EntriesSection />
+			</main>
+
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
--- react-state-1/react-state-1_journal-app-favorite-button/src/components/FavoriteButton/index.jsx
+++ react-state-1/react-state-1_journal-app-favorite-button/src/components/FavoriteButton/index.jsx
@@ -1,20 +1,21 @@
-import "./FavoriteButton.css";
-import StarFilled from "./star-filled.svg?react";
-import Star from "./star.svg?react";
+import { useState } from 'react';
+
+import './FavoriteButton.css';
+import StarFilled from './star-filled.svg?react';
+import Star from './star.svg?react';
 
 export default function FavoriteButton() {
-  // This should be a state variable.
-  const isFavorite = false;
+	// const isFavorite = false;// This should be a state variable.
+	const [isFavorite, setIsFavorite] = useState(false);
 
-  return (
-    <button
-      className="favorite-button"
-      onClick={() => {
-        console.log("favorite button clicked");
-      }}
-      aria-label="favorite"
-    >
-      {isFavorite ? <StarFilled /> : <Star />}
-    </button>
-  );
+	return (
+		<button
+			className="favorite-button"
+			//onClick={() => {console.log('favorite button clicked');}}
+			onClick={() => setIsFavorite(!isFavorite)}
+			aria-label="favorite"
+		>
+			{isFavorite ? <StarFilled /> : <Star />}
+		</button>
+	);
 }
