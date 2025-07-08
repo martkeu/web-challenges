7c8fe292c10ec7864933ca09aef14720e374a1df
Martin <martkeu@web.de>
2025-07-08 22:11:43 +0200

    solve challenge react-state-3_journal-app-favorite-button-shared-state

--- react-state-3/react-state-3_journal-app-favorite-button-shared-state/src/components/FavoriteButton/index.jsx
+++ react-state-3/react-state-3_journal-app-favorite-button-shared-state/src/components/FavoriteButton/index.jsx
@@ -1,20 +1,22 @@
-import { useState } from "react";
-import "./FavoriteButton.css";
-import StarFilled from "./star-filled.svg?react";
-import Star from "./star.svg?react";
+// import { useState } from 'react';
+import './FavoriteButton.css';
+import StarFilled from './star-filled.svg?react';
+import Star from './star.svg?react';
 
-export default function FavoriteButton() {
-  const [isFavorite, setIsFavorite] = useState(false);
+export default function FavoriteButton({ onToggleFavorite, isFavorite, id }) {
+	// const [isFavorite, setIsFavorite] = useState(false);
 
-  return (
-    <button
-      className="favorite-button"
-      onClick={() => {
-        setIsFavorite(!isFavorite);
-      }}
-      aria-label="favorite"
-    >
-      {isFavorite ? <StarFilled /> : <Star />}
-    </button>
-  );
+	return (
+		<button
+			className="favorite-button"
+			// onClick={() => {
+			// 	setIsFavorite(!isFavorite);
+            // }}
+            //Click-Handling for Favorite-Btn --mk
+            onClick={() => onToggleFavorite(id)}
+			aria-label="favorite"
+		>
+			{isFavorite ? <StarFilled /> : <Star />}
+		</button>
+	);
 }
