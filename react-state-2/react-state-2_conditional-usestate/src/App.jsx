defdc644ce82c1727b09f14d45918a6bf02296c8
Martin <martkeu@web.de>
2025-07-08 15:30:12 +0200

    solve challenge react-state-2_conditional-usestate

--- react-state-2/react-state-2_conditional-usestate/src/App.jsx
+++ react-state-2/react-state-2_conditional-usestate/src/App.jsx
@@ -1,50 +1,49 @@
-import "./styles.css";
-import { useState } from "react";
+import './styles.css';
+import { useState } from 'react';
 
 export default function App() {
-  const [showMessage, setShowMessage] = useState(false);
+	const [showMessage, setShowMessage] = useState(false);
+	const [message, setMessage] = useState('Secret message incoming...');
 
-  if (!showMessage) {
-    return (
-      <div className="container">
-        <h1>
-          Message is hidden{" "}
-          <span role="img" aria-label="scream">
-            😱
-          </span>
-        </h1>
-        <button
-          type="button"
-          className="button"
-          onClick={() => setShowMessage(true)}
-        >
-          Show Message
-        </button>
-      </div>
-    );
-  }
+	if (!showMessage) {
+		return (
+			<div className="container">
+				<h1>
+					Message is hidden{' '}
+					<span role="img" aria-label="scream">
+						😱
+					</span>
+				</h1>
+				<button
+					type="button"
+					className="button"
+					onClick={() => setShowMessage(true)}
+				>
+					Show Message
+				</button>
+			</div>
+		);
+	}
 
-  const [message, setMessage] = useState("Secret message incoming...");
-
-  return (
-    <div className="container">
-      <button
-        type="button"
-        className="button"
-        onClick={() => setShowMessage(false)}
-      >
-        Hide Message
-      </button>
-      <button
-        type="button"
-        className="button"
-        onClick={() =>
-          setMessage("The secret of Monkey Island is ... wait, no spoilers!")
-        }
-      >
-        Now really show the message!
-      </button>
-      <h2>{message}</h2>
-    </div>
-  );
+	return (
+		<div className="container">
+			<button
+				type="button"
+				className="button"
+				onClick={() => setShowMessage(false)}
+			>
+				Hide Message
+			</button>
+			<button
+				type="button"
+				className="button"
+				onClick={() =>
+					setMessage('The secret of Monkey Island is ... wait, no spoilers!')
+				}
+			>
+				Now really show the message!
+			</button>
+			<h2>{message}</h2>
+		</div>
+	);
 }
