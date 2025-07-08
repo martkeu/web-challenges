8923a0f49a18dd652fc24a514aef3b6352f12eca
Martin <martkeu@web.de>
2025-07-08 11:13:42 +0200

    solve challenge react-state-2_table-reservation

--- react-state-2/react-state-2_table-reservation/src/App.jsx
+++ react-state-2/react-state-2_table-reservation/src/App.jsx
@@ -1,12 +1,26 @@
-import Counter from "./components/Counter";
-import "./styles.css";
+import { useState } from 'react';
+import Counter from './components/Counter';
+import './styles.css';
 
 export default function App() {
-  return (
-    <div className="container">
-      <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
-    </div>
-  );
+	const [people, setPeople] = useState(0);
+
+	function addPeople() {
+		setPeople(people + 1);
+	}
+
+	function subtractPeople() {
+		setPeople(people - 1);
+	}
+
+	return (
+		<div className="container">
+			<h1>Place a Table Reservation</h1>
+
+			{/* <Counter people={people} setPeople={setPeople} /> */}
+			<Counter people={people} onAdd={addPeople} onSubtract={subtractPeople} />
+
+			<p>You are going to reserve a table for {people} people.</p>
+		</div>
+	);
 }
--- react-state-2/react-state-2_table-reservation/src/components/Counter.jsx
+++ react-state-2/react-state-2_table-reservation/src/components/Counter.jsx
@@ -1,29 +1,28 @@
-import { useState } from "react";
-
-export default function Counter() {
-  const [people, setPeople] = useState(0);
-
-  return (
-    <>
-      <h2>How many people would you like to visit us with?</h2>
-      <div className="counter__buttons-container">
-        <button
-          type="button"
-          className="counter__button"
-          aria-label="increment people count"
-          onClick={() => setPeople(people + 1)}
-        >
-          +
-        </button>
-        <button
-          type="button"
-          className="counter__button"
-          aria-label="decrement people count"
-          onClick={() => setPeople(people - 1)}
-        >
-          -
-        </button>
-      </div>
-    </>
-  );
+// export default function Counter({people, setPeople}) {
+export default function Counter({ onAdd, onSubtract }) {
+	return (
+		<>
+			<h2>How many people would you like to visit us with?</h2>
+			<div className="counter__buttons-container">
+				<button
+					type="button"
+					className="counter__button"
+					aria-label="increment people count"
+					//onClick={() => setPeople(people + 1)}
+					onClick={onAdd}
+				>
+					+
+				</button>
+				<button
+					type="button"
+					className="counter__button"
+					aria-label="decrement people count"
+					//onClick={() => setPeople(people - 1)}
+					onClick={onSubtract}
+				>
+					-
+				</button>
+			</div>
+		</>
+	);
 }
