e7f6b0b2546f2f4a567fbbaf7d3d3d22288865f3
Martin <martkeu@web.de>
2025-07-09 21:51:31 +0200

    solve challenge react-effects-and-fetch_iss-tracker

--- react-effects-and-fetch/react-effects-and-fetch_iss-tracker/src/App.jsx
+++ react-effects-and-fetch/react-effects-and-fetch_iss-tracker/src/App.jsx
@@ -1,26 +1,50 @@
-import { useState } from "react";
-import Controls from "./components/Controls";
-import Map from "./components/Map";
-import "./styles.css";
+import { useEffect, useState } from 'react';
+import Controls from './components/Controls';
+import Map from './components/Map';
+import './styles.css';
 
-const URL = "https://api.wheretheiss.at/v1/satellites/25544";
+const URL = 'https://api.wheretheiss.at/v1/satellites/25544';
 
 export default function App() {
-  const [coords, setCoords] = useState({
-    longitude: 0,
-    latitude: 0,
-  });
+	const [coords, setCoords] = useState({
+		longitude: 0,
+		latitude: 0,
+	});
 
-  async function getISSCoords() {}
+	async function getISSCoords() {
+		try {
+			const resp = await fetch(URL);
+			const data = await resp.json();
 
-  return (
-    <main>
-      <Map longitude={coords.longitude} latitude={coords.latitude} />
-      <Controls
-        longitude={coords.longitude}
-        latitude={coords.latitude}
-        onRefresh={getISSCoords}
-      />
-    </main>
-  );
+			console.log(data);
+
+			setCoords({
+				longitude: data.longitude,
+				latitude: data.latitude,
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	useEffect(() => {
+		const intervalID = setInterval(() => {
+			getISSCoords();
+		}, 5000);
+
+		//Cleanup Function
+		return () => clearInterval(intervalID);
+	}, []);
+
+	return (
+		<main>
+			<Map longitude={coords.longitude} latitude={coords.latitude} />
+
+			<Controls
+				longitude={coords.longitude}
+				latitude={coords.latitude}
+				onRefresh={getISSCoords}
+			/>
+		</main>
+	);
 }
