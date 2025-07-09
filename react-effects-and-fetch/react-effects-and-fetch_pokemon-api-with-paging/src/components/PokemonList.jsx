8afa73730db1d5747a09f68d04d18ca150f15e3d
Martin <martkeu@web.de>
2025-07-09 12:35:22 +0200

    solve challenge react-effects-and-fetch_pokemon-api-with-paging

--- react-effects-and-fetch/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.jsx
+++ react-effects-and-fetch/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.jsx
@@ -1,33 +1,55 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 export default function PokemonList() {
-  const [pokemon, setPokemon] = useState([]);
+	const [pokemon, setPokemon] = useState([]);
+	const [offset, setOffset] = useState(0);
 
-  useEffect(() => {
-    async function loadPokemon() {
-      try {
-        const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
-        );
-        const data = await response.json();
-        setPokemon(data.results);
-      } catch (error) {
-        console.log(error);
-      }
-    }
+	useEffect(() => {
+		async function loadPokemon() {
+			try {
+				const response = await fetch(
+					`https://pokeapi.co/api/v2/pokemon?offset=${offset}`
+				);
+				const data = await response.json();
 
-    loadPokemon();
-  }, []);
+				setPokemon(data.results);
+			} catch (error) {
+				console.log(error);
+			}
+		}
 
-  return (
-    <main>
-      <button type="button">Previous Page</button>
-      <button type="button">Next Page</button>
-      <ul>
-        {pokemon.map(({ name }) => (
-          <li key={name}>{name}</li>
-        ))}
-      </ul>
-    </main>
-  );
+		loadPokemon();
+	}, [offset]);
+
+	function nextPage() {
+		setOffset(offset + 20);
+	}
+
+	function prevPage() {
+		if (offset <= 20) {
+			setOffset(0);
+
+			return;
+		}
+
+		setOffset(offset - 20);
+	}
+
+	return (
+		<main>
+			<button type="button" onClick={prevPage} disabled={offset === 0}>
+				Previous Page
+			</button>
+
+			<button type="button" onClick={nextPage}>
+				Next Page
+			</button>
+
+			<ul>
+				{pokemon.map(({ name }) => (
+					<li key={name}>{name}</li>
+				))}
+			</ul>
+		</main>
+	);
 }
