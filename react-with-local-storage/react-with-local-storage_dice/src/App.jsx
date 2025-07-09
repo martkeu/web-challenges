eb550c3abba5c9e2afde17b513484113584a41cc
Martin <martkeu@web.de>
2025-07-09 17:42:19 +0200

    solve challenge react-with-local-storage_dice

--- react-with-local-storage/react-with-local-storage_dice/src/App.jsx
+++ react-with-local-storage/react-with-local-storage_dice/src/App.jsx
@@ -1,16 +1,29 @@
-import { D6Button } from "./components/D6Button";
-import History from "./components/History";
-import "./styles.css";
+import useLocalStorageState from 'use-local-storage-state';
+import { D6Button } from './components/D6Button';
+import { getD6Roll } from './utils';
+import History from './components/History';
+import './styles.css';
 
 export default function App() {
-  return (
-    <div className="app">
-      <main className="app__main">
-        <D6Button />
-      </main>
-      <aside className="app__aside">
-        <History rolls={[]} />
-      </aside>
-    </div>
-  );
+	const [rolls, setRolls] = useLocalStorageState('rolls', {
+		defaultValue: [],
+	});
+
+	const currentRollValue = rolls[0]?.value;
+
+	function handleRoll() {
+		setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
+	}
+
+	return (
+		<div className="app">
+			<main className="app__main">
+				<D6Button value={currentRollValue} onRoll={handleRoll} />
+			</main>
+
+			<aside className="app__aside">
+				<History rolls={rolls} />
+			</aside>
+		</div>
+	);
 }
