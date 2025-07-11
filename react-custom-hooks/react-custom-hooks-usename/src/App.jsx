75aa329e356223a9afa2e13a2e5fa088de6f8223
Martin <martkeu@web.de>
2025-07-11 10:45:06 +0200

    solve challenge react-custom-hooks-usename

--- react-custom-hooks/react-custom-hooks-usename/src/App.jsx
+++ react-custom-hooks/react-custom-hooks-usename/src/App.jsx
@@ -1,26 +1,32 @@
-import useName from "./hooks/useName";
-import "./styles.css";
+import useName from './hooks/useName';
+import './styles.css';
 
 export default function App() {
-  const [name, setFirstName, setLastName] = useName();
-  return (
-    <>
-      <form>
-        <input
-          type="text"
-          placeholder="first name"
-          value={null}
-          onChange={(event) => {}}
-        />
-        <input
-          type="text"
-          placeholder="last name"
-          value={null}
-          onChange={(event) => {}}
-        />
-      </form>
-      <h2>The full name:</h2>
-      <output>{name}</output>
-    </>
-  );
+	const [name, setFirstName, setLastName] = useName();
+
+	return (
+		<>
+			<form>
+				<input
+					type="text"
+					placeholder="first name"
+					value={null}
+					onChange={(event) => {
+						setFirstName(event.target.value);
+					}}
+				/>
+				<input
+					type="text"
+					placeholder="last name"
+					value={null}
+					onChange={(event) => {
+						setLastName(event.target.value);
+					}}
+				/>
+			</form>
+
+			<h2>The full name:</h2>
+			<output>{name}</output>
+		</>
+	);
 }
--- react-custom-hooks/react-custom-hooks-usename/src/hooks/useName.js
+++ react-custom-hooks/react-custom-hooks-usename/src/hooks/useName.js
@@ -1,3 +1,11 @@
+import { useState } from "react";
+
 export default function useName() {
-  return [null, null, null];
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+
+    const fullName = firstName + ' ' + lastName;
+
+
+  return [fullName, setFirstName, setLastName];
 }
