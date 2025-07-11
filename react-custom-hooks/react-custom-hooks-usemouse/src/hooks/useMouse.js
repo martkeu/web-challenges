e8c168bd543ec43451eeca6af5b6358b624ba03e
Martin <martkeu@web.de>
2025-07-11 11:19:42 +0200

    solve challenge react-custom-hooks-usemouse

--- react-custom-hooks/react-custom-hooks-usemouse/src/hooks/useMouse.js
+++ react-custom-hooks/react-custom-hooks-usemouse/src/hooks/useMouse.js
@@ -1,1 +1,17 @@
-export default function useMouse() {}
+import { useState, useEffect } from 'react';
+
+export default function useMouse() {
+	const [cursorPosition, setCursorPosition] = useState([0, 0]);
+
+	useEffect(() => {
+		const page = document.body;
+
+		page.addEventListener('mousemove', (event) => {
+			setCursorPosition([event.clientX, event.clientY]);
+		});
+	}, [cursorPosition]);
+
+	console.log(cursorPosition);
+
+	return cursorPosition;
+}
