741e676f8d8a3689ede42e5b9c8dd09803b9fba4
Martin <martkeu@web.de>
2025-07-21 17:48:26 +0200

    solve challenge react-styled-components_styled-navbar

--- react-styled-components/react-styled-components_styled-navbar/pages/_app.js
+++ react-styled-components/react-styled-components_styled-navbar/pages/_app.js
@@ -1,36 +1,51 @@
-import Link from "next/link";
-import GlobalStyle from "../styles";
-import styled from "styled-components";
+import Link from 'next/link';
+import GlobalStyle from '../styles';
+import styled from 'styled-components';
 
 const StyledNav = styled.nav`
-  border-bottom: solid 2px grey;
-  padding: 1rem;
+	border-bottom: solid 2px grey;
+	padding: 1rem;
 `;
 
 const StyledList = styled.ul`
-  list-style: none;
-  display: flex;
-  gap: 1rem;
+	list-style: none;
+	display: flex;
+	gap: 1rem;
+`;
+
+/*-----------------------------------------------------------------------------mk--
+| 
+|----------------------------------------------------------------------------------
+| 
+*/
+const StyledLink = styled(Link)`
+	text-decoration: none;
+
+	&:hover {
+		text-decoration: underline;
+	}
 `;
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <GlobalStyle />
-      <StyledNav>
-        <StyledList>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-          <li>
-            <Link href="/contact">Contact</Link>
-          </li>
-        </StyledList>
-      </StyledNav>
-      <Component {...pageProps} />
-    </>
-  );
+	return (
+		<>
+			<GlobalStyle />
+
+			<StyledNav>
+				<StyledList>
+					<li>
+						<StyledLink href="/">Home</StyledLink>
+					</li>
+					<li>
+						<StyledLink href="/about">About</StyledLink>
+					</li>
+					<li>
+						<StyledLink href="/contact">Contact</StyledLink>
+					</li>
+				</StyledList>
+			</StyledNav>
+
+			<Component {...pageProps} />
+		</>
+	);
 }
