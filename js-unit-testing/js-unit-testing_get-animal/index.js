1b8ad61a67bb1fb35af7e8b3cfc7bcf6206f49e4
Martin <martkeu@web.de>
2025-07-21 17:37:58 +0200

    solve challenge js-unit-testing_math-functions

--- js-unit-testing/js-unit-testing_get-animal/index.js
+++ js-unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,11 @@
-export function getAnimal() {}
+export function getAnimal(animals) {
+  if (animals === undefined) {
+    return "I do not like animals at all!";
+  }
+   
+   if (animals === "cats") {
+    return "I totally love cats!";
+  }
+   
+   return `I like ${animals}!`;
+}
