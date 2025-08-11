cb0d95b8d985505213f5bd7512ea4034292eafab
Martin <martkeu@web.de>
2025-08-11 11:27:02 +0200

    solve challenge backend-basics_routes

--- backend-basics/backend-basics_routes/server.js
+++ backend-basics/backend-basics_routes/server.js
@@ -1,3 +1,36 @@
-import { createServer } from "node:http";
+import { createServer } from 'node:http';
 
-// export const server = …
+export const server = createServer((req, res) => {
+	if (req.url === '/api/fish/1') {
+		res.statusCode = 200;
+		res.end('Shrimp');
+		return;
+	}
+
+	if (req.url === '/api/fish/2') {
+		res.statusCode = 200;
+		res.end('Anemonefish');
+		return;
+	}
+
+	res.statusCode = 404;
+	res.end('Not found');
+});
+
+// export const server = createServer((req, res) => {
+// 	switch (req.url) {
+// 		case '/api/fish/1':
+// 			res.statusCode = 200;
+// 			res.end('Shrimpp');
+// 			break;
+
+// 		case '/api/fish/2':
+// 			res.statusCode = 200;
+// 			res.end('Anemonefish');
+// 			break;
+
+// 		default:
+// 			res.statusCode = 404;
+// 			res.end('Not found');
+// 	}
+// });
