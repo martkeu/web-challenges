7f0764888abd685ba3b8d506fc1d44d7f24bdcbd
Martin <martkeu@web.de>
2025-08-11 12:31:24 +0200

    solve challenge backend-basics_chance

--- backend-basics/backend-basics_chance/server.js
+++ backend-basics/backend-basics_chance/server.js
@@ -1,3 +1,22 @@
-import { createServer } from "node:http";
+import { createServer } from 'node:http';
+import Chance from 'chance';
 
-// export const server = …
+function pleaseLetMeIntroduceMyself() {
+	const chance = new Chance();
+
+	return `
+      Hello, my name is ${chance.name()} and I am ${chance.age()} years old.\n 
+      I am a ${chance.profession()}.
+   `;
+}
+
+export const server = createServer((req, res) => {
+	if (req.url === '/') {
+		res.statusCode = 200;
+		res.end(pleaseLetMeIntroduceMyself());
+		return;
+	}
+
+	res.statusCode = 404;
+	res.end('Not found');
+});
