138c5d114d145203e02b82a673574591344a4a4f
Martin <martkeu@web.de>
2025-08-13 12:28:14 +0200

    solve challenge backend-create_products

--- backend-create/backend-create_products/components/ProductForm.js
+++ backend-create/backend-create_products/components/ProductForm.js
@@ -1,55 +1,96 @@
-import styled from "styled-components";
-import StyledButton from "@/components/Button";
+import styled from 'styled-components';
+import StyledButton from '@/components/Button';
+import useSWR from 'swr';
 
 export default function ProductForm() {
-  async function handleSubmit(event) {
-    event.preventDefault();
+   const { mutate } = useSWR('/api/products');
 
-    const formData = new FormData(event.target);
-    const productData = Object.fromEntries(formData);
-  }
+   /*-----------------------------------------------------------------------------mk--
+   | Daten senden
+   |----------------------------------------------------------------------------------
+   | - POST-Request an die Backend-API senden (Endpoint: /api/products)
+   | - Formular-Daten als body im JSON-Format mitgeben
+   | - Die API erstellt anhand der Daten einen neuen Eintrag in der DB
+   | - SWR-mutate() revalidiert und aktualisiert bei Änderungen die Daten (refetch)
+   */
+   async function onSubmit(data) {
+      const response = await fetch('/api/products', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		});
 
-  return (
-    <StyledForm onSubmit={handleSubmit}>
-      <StyledHeading>Add a new Fish</StyledHeading>
-      <StyledLabel htmlFor="name">
-        Name:
-        <input type="text" id="name" name="name" />
-      </StyledLabel>
-      <StyledLabel htmlFor="description">
-        Description:
-        <input type="text" id="description" name="description" />
-      </StyledLabel>
-      <StyledLabel htmlFor="price">
-        Price:
-        <input type="number" id="price" name="price" min="0" />
-      </StyledLabel>
-      <StyledLabel htmlFor="currency">
-        Currency:
-        <select id="currency" name="currency">
-          <option value="EUR">EUR</option>
-          <option value="USD">USD</option>
-          <option value="GBP">GBP</option>
-        </select>
-      </StyledLabel>
-      <StyledButton type="submit">Submit</StyledButton>
-    </StyledForm>
-  );
+		if (!response.ok) {
+			console.error(response.status);
+			return;
+		}
+
+      mutate();
+    }
+
+	async function handleSubmit(event) {
+		event.preventDefault();
+
+      //Formular-Daten ermitteln
+		const formData = new FormData(event.target);
+      const productData = Object.fromEntries(formData);
+
+      //Formular-Daten senden
+		await onSubmit(productData);
+      
+      //Formular leeren
+      event.target.reset();
+      event.target.name.focus();
+	}
+
+	return (
+		<StyledForm onSubmit={handleSubmit}>
+			<StyledHeading>Add a new Fish</StyledHeading>
+
+			<StyledLabel htmlFor="name">
+				Name:
+				<input type="text" id="name" name="name" />
+			</StyledLabel>
+
+			<StyledLabel htmlFor="description">
+				Description:
+				<input type="text" id="description" name="description" />
+			</StyledLabel>
+
+			<StyledLabel htmlFor="price">
+				Price:
+				<input type="number" id="price" name="price" min="0" />
+			</StyledLabel>
+
+			<StyledLabel htmlFor="currency">
+				Currency:
+				<select id="currency" name="currency">
+					<option value="EUR">EUR</option>
+					<option value="USD">USD</option>
+					<option value="GBP">GBP</option>
+				</select>
+			</StyledLabel>
+
+			<StyledButton type="submit">Submit</StyledButton>
+		</StyledForm>
+	);
 }
 
 export const StyledForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  gap: 5px;
+	display: flex;
+	flex-direction: column;
+	gap: 5px;
 `;
 
 export const StyledHeading = styled.h2`
-  text-align: center;
-  color: var(--color-nemo);
+	text-align: center;
+	color: var(--color-nemo);
 `;
 
 export const StyledLabel = styled.label`
-  display: flex;
-  justify-content: space-between;
-  gap: 5px;
+	display: flex;
+	justify-content: space-between;
+	gap: 5px;
 `;
--- backend-create/backend-create_products/pages/api/products/index.js
+++ backend-create/backend-create_products/pages/api/products/index.js
@@ -10,6 +10,18 @@ export default async function handler(request, response) {
     response.status(200).json(products);
     return;
   }
+   
+  if (request.method === 'POST') {
+		try {
+			const productData = request.body;
+
+			await Product.create(productData);
+
+			return response.status(201).json({ status: 'Product created.' });
+		} catch (error) {
+			return response.status(400).json({ error: error.message });//Bad Request -> Clientfehler
+		}
+	}
 
   response.status(405).json({ status: "Method not allowed." });
 }
